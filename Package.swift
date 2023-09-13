30844abb305bb4469b259676ccde6c9530f4bd9e
disha <disha.argon@gmail.com>
2023-09-13 18:02:54 +0530

    initial

--- Package.swift
+++ Package.swift
@@ -24,16 +24,23 @@ import PackageDescription
 //)
 
 let package = Package(
-    name: "CrispiOSPlugin", dependencies: [
-    .package(url: "https://github.com/crisp-im/crisp-sdk-ios.git", .upToNextMajor(from: "2.0.0")),
-  ],
-  targets: [
-    .target(
-      name: "CrispiOSPlugin",
-      dependencies: [
-        .product(name: "Crisp", package: "crisp-sdk-ios")
-        // or: .productItem(name: "Crisp", package: "crisp-sdk-ios")
-      ]
-    )
-  ]
+    name: "CrispiOSPlugin",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "CrispiOSPlugin",
+            targets: ["CrispiOSPlugin"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/crisp-im/crisp-sdk-ios.git", .upToNextMajor(from: "2.0.0")),
+    ],
+    targets: [
+        .target(
+            name: "CrispiOSPlugin",
+            dependencies: [
+                .product(name: "Crisp", package: "crisp-sdk-ios")
+                // or: .productItem(name: "Crisp", package: "crisp-sdk-ios")
+            ]
+        )
+    ]
 )
